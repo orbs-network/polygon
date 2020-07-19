916baf2be9b3ba47522397b05c3609dc3d41e75f
Kirill Maksimov <kirill@orbs.com>
2020-07-19 12:21:06 +0300

    Remove obsolete commands

--- bin/polygon.js
+++ bin/polygon.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { create, destroy, update, status, bootstrapConfig } = require("../lib/cli");
-const { createOptions, destroyOptions, updateOptions, networkStatusOptions, generateConfigOptions } = require("../lib/cli/options");
+const { create, destroy } = require("../lib/cli");
+const { createOptions, destroyOptions } = require("../lib/cli/options");
 
 require("yargs") // eslint-disable-line
   .command("create", "Create an Orbs node in the cloud", createOptions, create)
@@ -10,9 +10,6 @@ require("yargs") // eslint-disable-line
     alias: "v",
     default: false
   })
-  .command("update", "Updates a running node's topology and vChains version", updateOptions, update)
-  .command("network-status", "Shows status of the whole network", networkStatusOptions, status)
-  .command("bootstrap-config", "Generates bootstrap config", generateConfigOptions, bootstrapConfig)
   .demandCommand()
   .help()
   .argv
