70747a941e57c95b65b04cc319a7807d35970fdc
uvorbs <yuval@orbs.com>
2025-06-29 16:01:33 +0300

    use dynamic ubuntu 18 ami

--- resources/terraform/aws/ubuntu-ami.tf
+++ resources/terraform/aws/ubuntu-ami.tf
@@ -1,13 +1,4 @@
-data "aws_ami" "ubuntu-18_04" {
-  most_recent = true
-  owners = ["${var.ubuntu_account_number}"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
-
-variable "ubuntu_account_number" {
-  default = "099720109477"
+# SSM lookup (already present)
+data "aws_ssm_parameter" "ubuntu_18_04" {
+  name = "/aws/service/canonical/ubuntu/server/18.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
